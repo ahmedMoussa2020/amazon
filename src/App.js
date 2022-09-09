ee64272e1cc10e0e79bc2005f3a940e1f62d4e3b
Ahmed <>
2022-09-09 17:25:21 +0300

    payment page

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import Login from "./Login";
 // import { useEffect } from "react";
 import { auth } from "./firebase";
 import { useStateValue } from "./StateProvider";
+import Payment from "./Payment";
 
 function App() {
   const [{}, dispatch] = useStateValue();
@@ -48,6 +49,10 @@ function App() {
           <Route path="/checkout">
             <Header />
             <Checkout />
+          </Route>
+          <Route path="/payment">
+            <Header />
+            <Payment />
           </Route>
           <Route path="/">
             <Header />
